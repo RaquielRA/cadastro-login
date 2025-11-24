a444a5799618e76c8057634850b6cc94b1a298ba
Raquiel Ribeiro <ribeiro.raquiel@gmail.com>
2025-11-24 20:19:33 -0300

    feat(db-setup): Implementa Entidade Usuario e persistência Jpa/Hibernate;corrige .gitignore.

---
+++ src/main/java/com/raquielra/cadastrologin/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.raquielra.cadastrologin.repository;
+
+import com.raquielra.cadastrologin.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+}
